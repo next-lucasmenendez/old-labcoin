9812ca74246f95d45aa5c0a5936def423781aca7
beeva-carlosgonzalez <carlos.gonzalez@beeva.com>
2018-05-28 09:06:53 +0200

    added qr scanning

--- app/components/qr-reader.js
+++ app/components/qr-reader.js
@@ -2,14 +2,15 @@ const QRReader = Vue.component("qr-reader", {
 	template: `<video id="preview"></video>`,
 
 	mounted() {
-        let scanner = new Instascan.Scanner({continuous: true, video: document.getElementById('preview'), backgroundScan: true,  scanPeriod: 1
-,            mirror: true});
+        let scanner = new Instascan.Scanner({continuous: true, video: document.getElementById('preview'),
+            backgroundScan: true, scanPeriod: 1, mirror: true});
         scanner.addListener('scan', function (content) {
+
             console.log(content);
         });
         Instascan.Camera.getCameras().then(function (cameras) {
             if (cameras.length > 0) {
-                scanner.start(cameras[0]).catch(console.log);
+                scanner.start(cameras[1]).catch(console.log);
             } else {
                 console.error('No cameras found.');
             }
