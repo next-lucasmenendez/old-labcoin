41940f4883b2173d5867049b9dfecbbf3204e904
Lucas Menendez <epucas@gmail.com>
2018-05-29 13:35:01 +0200

    first deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+	provider: "https://ni1sdo2g13.execute-api.eu-west-1.amazonaws.com/dev/enode/",
+	contractUri: "https://ni1sdo2g13.execute-api.eu-west-1.amazonaws.com/dev/json",
+	contractAddress: "0xba706e5d6f9406910e649f04c6e6bc1fc0b66bd7",
+	tokensATM: "https://qczr5ylzki.execute-api.eu-west-1.amazonaws.com/latest/recharge"
+}
